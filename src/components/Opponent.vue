45bd99f990f409b0d6ea089cb94f945e428a3b6e
Mary Brady <emceebrady@gmail.com>
2018-08-31 10:51:47 -0600

    back of card showing

--- src/components/Opponent.vue
+++ src/components/Opponent.vue
@@ -6,14 +6,15 @@
         </div>
         <div class="card-deck">
             <div v-for="card in myOpponent.hand" :key="card.name">
-                <div class="card border-dark mb-3" style="width: 10rem;">
-                    <img class="card-img-top" :src="card.img" alt="">
-                    <div class="card-body">
+                    <div class="card border-dark mb-3" style="width: 10rem;">
+                        <img v-if="!card.visible" src="../assets/card.png" alt="back of card" class="card-back">
+                    <img v-if="card.visible" class="card-img-top" :src="card.img" alt="">
+                    <div v-if="card.visible" class="card-body">
                         <h5 class="card-title">
                             <b>{{card.name}}</b>
                         </h5>
                     </div>
-                    <ul class="list-group list-group-flush">
+                    <ul v-if="card.visible" class="list-group list-group-flush">
                         <li class="list-group-item">
                             <b>Attack</b>: {{card.attack}}</li>
                         <li class="list-group-item">
@@ -34,37 +35,41 @@
 </template>
 
 <script>
-    export default {
-        name: "Opponent",
-        props: {
-            msg: String
-        },
-        computed: {
-            myOpponent() {
-                return this.$store.state.game.players[1];
-            }
-        },
-        methods: {
-            setOpponentCard(card) {
-                this.$store.dispatch("setOpponentCard", card);
-            }
-        }
-    };
+export default {
+  name: "Opponent",
+  props: {
+    msg: String
+  },
+  computed: {
+    myOpponent() {
+      return this.$store.state.game.players[1];
+    }
+  },
+  methods: {
+    setOpponentCard(card) {
+      this.$store.dispatch("setOpponentCard", card);
+    }
+  }
+};
 </script>
 
 <style scoped>
-    .opponent-view {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-    }
+.opponent-view {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
 
-    .name-stuff {
-        margin: 5px;
-    }
+.name-stuff {
+  margin: 5px;
+}
 
-    .card-deck {
-        display: flex;
-    }
+.card-deck {
+  display: flex;
+}
+
+.card-back {
+  max-height: 300px;
+}
 </style>
